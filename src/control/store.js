638aa9abf7176469d93b3e42679c31a67ab42644
pacificescape <17690123+pacificescape@users.noreply.github.com>
2021-03-11 14:31:48 +0100

    .

--- src/control/store.js
+++ src/control/store.js
@@ -5,8 +5,8 @@ const clearMap = createEvent();
 
 export const mapStore = createStore({ elevation: [], moisture: [] })
   .on(addMap, (state, map) => {
-    console.log(state);
-    console.log(mapStore);
+    // console.log(state);
+    // console.log(mapStore);
     return map;
   })
   .reset(clearMap);
@@ -15,7 +15,6 @@ export const connectEvt = createEvent();
 export const connetionStore = createStore({ status: 0 }).on(
   connectEvt,
   (state, connection) => {
-    console.log(state, connection);
     return { ...state, status: connection };
   }
 );
